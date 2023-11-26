0f07d4282f5d1eb5b49ad6becd6ecfda512caaec
Brezker <brezker10@gmail.com>
2023-11-26 09:42:56 -0600

    enemigo muere despues de 10 seg

---
+++ Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Enemy/EnemyController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+    private Animator anim;
+    private float life;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = 3f;
+        life = 100f;
+        InvokeRepeating(nameof(ApplyDamage), 1.0f, 1.0f);
+
+    }
+    private void ApplyDamage()
+    {
+        TakeDamage(10f);
+    }
+
+
+    void Update()
+    {
+        // Podemos hacer un if si consideramos que el personaje puede estar sin moverse hasta cierto momento
+        agent.SetDestination(target.position);
+        anim.SetFloat("Moves", 1f);
+        // Agregar un if take damage
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        // Resta el daño de la vida actual
+        life -= 10f;  // Reducción de la vida
+
+        // Puedes agregar más lógica aquí, por ejemplo, activar una animación de recibir daño.
+        //animator.Play(HasAmmunition() ? "Reload" : "Reload Empty", 0, 0.0f);
+
+
+        // Verifica si el enemigo ha quedado sin vida
+        if (life <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        anim.SetFloat("Moves", 0f);
+        anim.SetFloat("Dies", 1f); // Detener la animación de movimiento
+        anim.SetBool("isDead", true); // Puedes tener otro parámetro para la animación de muerte si es necesario
+        agent.isStopped = true; // Detener el NavMeshAgent
+
+        Collider enemyCollider = GetComponent<Collider>();
+        if (enemyCollider != null)
+        {
+            enemyCollider.enabled = false;
+        }
+        
+        Destroy(gameObject, 9f);
+    }
+}
